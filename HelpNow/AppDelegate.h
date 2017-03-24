55f4016f7f042c8ab208142b785f8ad764330440
Rob <rgklouda@comast.net>
2017-03-24 08:01:29 -0400

    Initial Commit

---
+++ HelpNow/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HelpNow
+//
+//  Created by Robert Klouda on 3/24/17.
+//  Copyright © 2017 Robert Klouda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
